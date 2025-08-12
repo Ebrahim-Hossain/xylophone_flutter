c21ee38d9724a63337e4187ab573d49f6049450d
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-08-12 20:33:51 +0600

    added buttons

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -18,6 +18,43 @@ class HomeScreen extends StatelessWidget {
                     player.play(AssetSource("note1.wav"));
                   },
                   child: Container(height: 30,width: double.infinity,color: Colors.red,)),
+              TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource("note2.wav"));
+                  },
+                  child: Container(height: 30,width: double.infinity,color: Colors.yellow,)),
+              TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource("note3.wav"));
+                  },
+                  child: Container(height: 30,width: double.infinity,color: Colors.grey,)),
+              TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource("note4.wav"));
+                  },
+                  child: Container(height: 30,width: double.infinity,color: Colors.green,)),
+
+              TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource("note5.wav"));
+                  },
+                  child: Container(height: 30,width: double.infinity,color: Colors.orange,)),
+              TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource("note6.wav"));
+                  },
+                  child: Container(height: 30,width: double.infinity,color: Colors.blue,)),
+              TextButton(
+                  onPressed: () {
+                    final player = AudioPlayer();
+                    player.play(AssetSource("note7.wav"));
+                  },
+                  child: Container(height: 30,width: double.infinity,color: Colors.teal,)),
 
             ],
           ),
