df2e96fa8c8ff6f987b497b4b2d60697126f040a
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-08-15 15:47:15 +0600

    complete

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,61 +1,43 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+
+  void onClick(int number) {
+    final player = AudioPlayer();
+    player.play(AssetSource("note$number.wav"));
+  }
+
+ Widget colorLine({required color,required int number}) {
+
+   var result = Expanded(
+      child: InkWell(
+        onTap: () {
+          onClick(number);
+        },
+        child: Container(color: color),
+      ),
+    );
+   return result;
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Center(
           child: Column(
             children: [
-              TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource("note1.wav"));
-                  },
-                  child: Container(height: 30,width: double.infinity,color: Colors.red,)),
-              TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource("note2.wav"));
-                  },
-                  child: Container(height: 30,width: double.infinity,color: Colors.yellow,)),
-              TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource("note3.wav"));
-                  },
-                  child: Container(height: 30,width: double.infinity,color: Colors.grey,)),
-              TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource("note4.wav"));
-                  },
-                  child: Container(height: 30,width: double.infinity,color: Colors.green,)),
-
-              TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource("note5.wav"));
-                  },
-                  child: Container(height: 30,width: double.infinity,color: Colors.orange,)),
-              TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource("note6.wav"));
-                  },
-                  child: Container(height: 30,width: double.infinity,color: Colors.blue,)),
-              TextButton(
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(AssetSource("note7.wav"));
-                  },
-                  child: Container(height: 30,width: double.infinity,color: Colors.teal,)),
-
+              colorLine( color: Colors.red, number: 1),
+              colorLine( color: Colors.yellow, number: 2),
+              colorLine( color: Colors.grey, number: 3),
+              colorLine( color: Colors.green, number: 4),
+              colorLine( color: Colors.orange, number: 5),
+              colorLine( color: Colors.blue, number: 6),
+              colorLine( color: Colors.teal, number: 7),
             ],
           ),
         ),
